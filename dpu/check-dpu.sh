72f050e2a40869b6d283393bb8fcf4a58d63a399
Marcel Wiget <mwiget@gmail.com>
2025-09-01 07:38:11 +0000

    survive dpu reboot

--- dpu/check-dpu.sh
+++ dpu/check-dpu.sh
@@ -2,4 +2,4 @@
 
 node=$1
 [ -z "$node" ] && { echo "Usage: $0 <node>"; exit 1; }
-ssh $node "sudo ovs-vsctl show; ip -br l; cat /proc/net/bonding/bond0; ifconfig -a |grep mtu; sudo ovs-vsctl get Open_vSwitch . other_config:hw-offload"
+ssh $node "sudo ovs-vsctl show; ip -br l; cat /proc/net/bonding/bond0; ifconfig -a |grep mtu; echo "hw-offload: "; sudo ovs-vsctl get Open_vSwitch . other_config:hw-offload; sudo ovs-vsctl show"
