a615c1cd98a6597a7bf30b3ad2ecf0c26cd015ff
Marcel Wiget <mwiget@gmail.com>
2025-09-03 18:14:43 +0200

    adjust max mtu for tmm to 9000, add sriov vf allocatables and nad

--- scripts/decode-jwt.sh
+++ scripts/decode-jwt.sh
@@ -34,22 +34,3 @@ fi
 f1=$(cut -d\. -f1 $1)
 echo $f1 | base64 -d | jq
 echo ""
-
-f2=$(cut -d\. -f2 $1)
-decoded=$(echo $f2 | base64 -d)
-echo $decoded | jq
-echo ""
-
-# Extract and print
-iat=$(echo "$decoded" | jq -r '.iat')
-f5_sat=$(echo "$decoded" | jq -r '.f5_sat')
-
-echo "iat    (UTC): $(date -u -d "@$iat")"
-echo "f5_sat (UTC): $(date -u -d "@$f5_sat")"
-
-now=$(date +%s)
-echo ""
-if (( f5_sat < now )); then
-  echo "ERROR: jwt token has expired!"
-  exit 2
-fi
--- scripts/restart-sriovdp.sh
+++ scripts/restart-sriovdp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl delete -f resources/sriovdp-config.yaml
 kubectl apply -f resources/sriovdp-config.yaml
-kubectl -n kube-system rollout restart ds/kube-sriov-device-plugin-amd64
+kubectl -n kube-system rollout restart ds/kube-sriov-device-plugin
